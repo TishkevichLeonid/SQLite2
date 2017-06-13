80b83e048d6ca77399b099f4e3d7d1db715c2741
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-06-13 16:58:16 +0300

    finish

--- app/src/main/java/com/leo/android/sqlite2/MainActivity.java
+++ app/src/main/java/com/leo/android/sqlite2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.leo.android.sqlite2;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText mFunctionText, mPopulationText, mGroupPopulationText;
     private RadioGroup mRadioGroup;
 
-    private SQLiteDatabase db;
+    private SQLiteDatabase mDatabase;
     private DBHelper mDBHelper;
 
     @Override
@@ -58,17 +60,131 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mDBHelper = new DBHelper(this);
         //подключаемся к базе
-        mDBHelper.getWritableDatabase();
+        mDatabase =  mDBHelper.getWritableDatabase();
+
+        //проверка существования записей
+        Cursor cursor = mDatabase.query("myTable", null, null, null, null, null, null);
+        if (cursor.getCount() == 0){
+            ContentValues contentValues = new ContentValues();
+            //заполняем таблицу
+            for (int i = 0; i < 10; i++){
+                contentValues.put("name", name[i]);
+                contentValues.put("people", people[i]);
+                contentValues.put("region", region[i]);
+                Log.d(LOG_TAG, "id = " + mDatabase.insert("myTable", null, contentValues));
+            }
+
+        }
+        cursor.close();
+        mDBHelper.close();
+        //эмулируем нажатие кнопки mAllBtn
+        onClick(mAllBtn);
 
     }
 
     @Override
     public void onClick(View view) {
 
+        //подключаемся к базе
+        mDatabase = mDBHelper.getWritableDatabase();
+
+        //читаем данные с полей
+        String functionText = mFunctionText.getText().toString();
+        String populationText = mPopulationText.getText().toString();
+        String groupPopulationText = mGroupPopulationText.getText().toString();
+
+        // переменные для Query
+        String[] columns = null; // по какой функции проводится выборка элементов
+        String selection = null;
+        String[] selectionArgs = null;
+        String groupBy = null;
+        String having = null;
+        String orderBy = null;
+
+        //курсор
+        Cursor cursor = null;
+
+        switch (view.getId()){
+            //все записи
+            case R.id.All_context:
+                Log.d(LOG_TAG, "--- Все записи ---");
+                cursor = mDatabase.query("myTable", null, null, null, null, null, null);
+                break;
+            //функция
+            case R.id.Function_Button:
+                Log.d(LOG_TAG, "--- Функция ");
+                columns = new String[] {functionText};
+                cursor = mDatabase.query("myTable", columns, null, null, null, null, null);
+                break;
+            //население больше чем
+            case R.id.Population_Button:
+                Log.d(LOG_TAG, "--- Насление больше ");
+                selection = "people > ?";
+                selectionArgs = new String[] {populationText};
+                cursor = mDatabase.query("myTable", null, selection, selectionArgs, null, null, null);
+                break;
+            //Население по региону
+            case R.id.Population_Group:
+                Log.d(LOG_TAG, "--- Население по региону ---");
+                columns = new String[] {"region", "sum(people) as people"};
+                groupBy = "region";
+                cursor = mDatabase.query("myTable", columns, null, null, groupBy, null, null);
+                break;
+            //Население по региону больше чем
+            case R.id.Population_Group_Count:
+                Log.d(LOG_TAG, "--- Регионы с населением больше " + groupPopulationText + " ---");
+                columns = new String[] {"region", "sum(people) as people"};
+                groupBy = "region";
+                having = "sum(people) > " + groupPopulationText;
+                cursor = mDatabase.query("myTable", columns, null, null, groupBy, having, null);
+                break;
+            //Сортировка
+            case R.id.Sorting_Button:
+                //сортировка по
+                switch (mRadioGroup.getCheckedRadioButtonId()){
+
+                    //наименованию
+                    case R.id.radio_Name:
+                        Log.d(LOG_TAG, "--- Сортировка по наименованию ---");
+                        orderBy = "name";
+                        break;
+                    //населениею
+                    case R.id.Radio_Population:
+                        Log.d(LOG_TAG, "--- Сортировка по населению ---");
+                        orderBy = "people";
+                        break;
+                    //региону
+                    case R.id.Radio_Region:
+                        Log.d(LOG_TAG, "--- Сортировка по региону ---");
+                        orderBy = "region";
+                        break;
+
+                }
+
+                cursor = mDatabase.query("myTable", null, null, null, null, null, orderBy);
+                break;
+        }
+
+        if (cursor != null){
+            if (cursor.moveToFirst()){
+                String str;
+                do {
+                    str = "";
+                    for (String cn : cursor.getColumnNames()){
+                        str = str.concat(cn + " = " + cursor.getString(cursor.getColumnIndex(cn)) + "; ");
+                    }
+                    Log.d(LOG_TAG, str);
+                } while (cursor.moveToNext());
+            }
+
+            cursor.close();
+        } else Log.d(LOG_TAG, "Cursor is null");
+
+        mDBHelper.close();
+
     }
 
     class DBHelper extends SQLiteOpenHelper {
-
 
         public DBHelper(Context context) {
             super(context, "myDB", null, 1);
@@ -78,8 +194,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void onCreate(SQLiteDatabase sqLiteDatabase) {
             Log.d(LOG_TAG, "--- onCreate database ---");
             //создаем таблицу с полями
-            db.execSQL("create tabble myTable (" + "id integer primary key autoincrement, "
-            + "name text, " + "people integer, " + "region text" + ");");
+            sqLiteDatabase.execSQL("create table myTable (" + "id integer primary key autoincrement, "
+            + "name text, " + "people integer, " + "region text" + ") ;");
 
         }
 
